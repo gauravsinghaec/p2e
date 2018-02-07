0db5c3eb87a5e50092ab16ef570127dafc5a3b5f
Gaurav Singh <gauravsinghaec@gmail.com>
2018-02-07 16:44:33 +0530

    Update loadLocationData.sql file

--- loadLocationData.sql
+++ loadLocationData.sql
@@ -7,3 +7,15 @@ INSERT INTO locations values (6,'Ajmer, Rajasthan',' 26.449896',' 74.639915');
 INSERT INTO locations values (7,'Udaipur, Rajasthan',' 24.571270',' 73.691544');
 INSERT INTO locations values (8,'Pokharan, Rajasthan',' 26.920542',' 71.916550');
 INSERT INTO locations values (9,'Pali, Rajasthan',' 25.771315',' 73.323685');
+
+
+
+heroku pg:psql -c "INSERT INTO locations values (1,'Amer Fort, Jaipur',' 26.996471',' 75.876472');"
+heroku pg:psql -c "INSERT INTO locations values (2,'Jodhpur, Rajasthan',' 26.263863',' 73.008957');"
+heroku pg:psql -c "INSERT INTO locations values (3,'Pushkar, Rajasthan',' 26.489679',' 74.550941');"
+heroku pg:psql -c "INSERT INTO locations values (4,'Jaipur, Rajasthan',' 26.922070',' 75.778885');"
+heroku pg:psql -c "INSERT INTO locations values (5,'Jaisalmer, Rajasthan',' 26.911661',' 70.922928');"
+heroku pg:psql -c "INSERT INTO locations values (6,'Ajmer, Rajasthan',' 26.449896',' 74.639915');"
+heroku pg:psql -c "INSERT INTO locations values (7,'Udaipur, Rajasthan',' 24.571270',' 73.691544');"
+heroku pg:psql -c "INSERT INTO locations values (8,'Pokharan, Rajasthan',' 26.920542',' 71.916550');"
+heroku pg:psql -c "INSERT INTO locations values (9,'Pali, Rajasthan',' 25.771315',' 73.323685');"
